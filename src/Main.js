48a95d0865f0321dbcc6d6f11f12df2e87154367
Krieghan Riley <kriley@ngmoco.com>
2012-09-11 13:28:55 -0700

    Initial version, which does not work

---
+++ src/Main.js
@@ -0,0 +1,47 @@
+var Core = require('../NGCore/Client/Core');
+var GL2 = require('../NGCore/Client/GL2');
+var UI = require('../NGCore/Client/UI');
+
+console.log('parsing Main.js');
+
+/*
+
+var addBackgroundImage = function(width, height)
+{
+    var bgSprite = new GL2.Sprite();
+    bgSprite.setImage("assets/background.png", [width, height], [0, 0]);
+    GL2.Root.addChild(bgSprite);
+}
+
+var addText = function(width, height)
+{
+    var label = new GL2.Text();
+    label.setSize(width, height);
+    label.setAnchor(0, 0);
+    label.setText('Dzien Dobry');
+    label.setColor(0, 0, 0);
+    GL2.Root.addChild(label);
+}
+
+*/
+
+function main()
+{
+    console.log('starting main');
+    
+    /*
+    var glView = new UI.GLView();
+    var width = UI.Window.getWidth();
+    var height = UI.Window.getHeight();
+    glView.setFrame(0, 0, width, height);
+    
+    glView.setOnLoad(function()
+    {
+        console.log('adding background');
+        addBackgroundImage(width, height);
+        addText(width, height);
+    });
+    
+    glView.setActive(true);
+    */
+}
